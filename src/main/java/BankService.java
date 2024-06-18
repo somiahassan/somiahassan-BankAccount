8358e6240eaa3b3004dcedf500ce9032f7b4175e
Somia Farag <somiafarag5@gmail.com>
2024-06-18 03:18:38 +0000

    lab completed

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,6 +23,9 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        if(amount >0){
+        this.balance += amount;
+        }
 
     }
 
@@ -32,7 +35,9 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(amount > 0 && this.balance >= amount) {
+            this.balance -= amount;
+        }
     }
 
     /**
@@ -40,6 +45,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
